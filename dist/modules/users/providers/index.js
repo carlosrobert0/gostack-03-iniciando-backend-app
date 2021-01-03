476b5dfbd18e32f3848fcbb2ed39663b8d6daccd
carlosrobert0 <carlosroberto15755@gmail.com>
2021-01-03 00:28:57 -0300

    Ajustes gitignore

--- dist/modules/users/providers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _BCrypHashProvider = _interopRequireDefault(require("./HashProvider/implementations/BCrypHashProvider"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_tsyringe.container.registerSingleton('HashProvider', _BCrypHashProvider.default);
